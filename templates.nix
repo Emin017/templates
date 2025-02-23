82ebd65c1feafaad81119944eb0f6e96e5f7eb7a
Qiming Chu <cchuqiming@gmail.com>
2025-02-23 09:39:56 +0800

    [style] reformat templates.nix
    Signed-off-by: Qiming Chu <cchuqiming@gmail.com>

--- templates.nix
+++ templates.nix
@@ -3,10 +3,10 @@
     path = ./rust;
     description = "Basic rust project setup without flake";
   };
-	"scala" = {
-		path = ./scala;
-		description = "Basic scala project setup";
-	};
+  "scala" = {
+    path = ./scala;
+    description = "Basic scala project setup";
+  };
   "ocaml" = {
     path = ./ocaml;
     description = "Basic ocaml project setup";
